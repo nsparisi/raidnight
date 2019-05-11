c8cf37d3fe2d77091ce05a80ddf082d5390098c5
Nick Parisi <nsparisi@gmail.com>
2019-05-11 13:02:21 -0700

    input tokenizes and supports loops

--- code/input.ts
+++ code/input.ts
@@ -6,6 +6,13 @@ module RaidNight.Engine
         actions: Action[];
     } 
 
+    enum TokenType { Wait, Loop, Arg_Open, Arg_Close, Block_Open, Block_Close, Skill_Operator, Alphabetic, Numeric }
+
+    interface Token {
+        token: string;
+        type: TokenType;
+    }
+
     export class Input
     {
         private generateErrorResult = (errorMessage: string): InputResult =>
@@ -40,7 +47,7 @@ module RaidNight.Engine
                 switch(inputs[i].id.toUpperCase())
                 {
                     case "WIZARD_INPUT": {
-                        let result = this.parseRawInput("WIZARD", inputs[i].value);
+                        let result = this.parseWrapper("WIZARD", inputs[i].value);
                         if(!result.hasError)
                         {
                             wizard.actionList = result.actions;
@@ -54,7 +61,7 @@ module RaidNight.Engine
                     }
                     
                     case "PRIEST_INPUT": {
-                        let result = this.parseRawInput("PRIEST", inputs[i].value);
+                        let result = this.parseWrapper("PRIEST", inputs[i].value);
                         if(!result.hasError)
                         {
                             priest.actionList = result.actions;
@@ -68,7 +75,7 @@ module RaidNight.Engine
                     }
                     
                     case "KNIGHT_INPUT": {
-                        let result = this.parseRawInput("KNIGHT", inputs[i].value);
+                        let result = this.parseWrapper("KNIGHT", inputs[i].value);
                         if(!result.hasError)
                         {
                             knight.actionList = result.actions;
@@ -93,105 +100,335 @@ module RaidNight.Engine
             }
         }
 
-        parseRawInput = (className: string, raw: string): InputResult =>
+        parseWrapper = (className: string, raw: string): InputResult => 
         {
-            // "Flare:Dragon,Fireball:Dragon,Move:Up,Fireball:Dragon";
-            let actions: Action[] = [];
+            try {
+                let actions = this.parse(className, raw);
+                return this.generateSuccessResult(actions);
+            }
+            catch (e) {
+                return this.generateErrorResult(e);
+            }
+        }
+
+        parse = (className: string, raw: string): Action[] =>
+        {
+            let seek = 0;
+            let tokens = this.tokenize(raw);
             let validTargets = ["DRAGON", "WIZARD", "PRIEST", "KNIGHT"];
 
-            console.log(`Parsing input ${raw}`);
-            
-            // join newlines into a single line
-            raw = raw.split("\r\n").join(" ").split("\r").join(" ").split("\n").join(" ");
-
-            let actionTokens = raw.split(" ");
-            let i = 0;
-            for (i = 0; i < actionTokens.length; i++)
+            let process_wait = (): Action =>
             {
-                let components = actionTokens[i].split(":")
+                seek++;
+                return new action_Wait();
+            }
+            
+            let process_action = (): Action =>
+            {
+                let action = tokens[seek++];
 
-
-                if (components.length == 1 && components[0].trim().length == 0)
+                if (action == null  || 
+                    action.type != TokenType.Alphabetic)
                 {
-                    // empty string
-                    continue;
+                    throw "ERROR P3";
                 }
 
-                if (components.length == 0 || components.length > 2)
+                if (action.token == "MOVE")
                 {
-                    return this.generateErrorResult("Input not in correct format.");
-                }
+                    let operator = tokens[seek++];
+                    let target = tokens[seek++];
 
-                let action = components[0].trim();
-
-                if (action.toUpperCase() == "WAIT")
-                {
-                    actions.push(new action_Wait());
-                    continue;
-                }
-                else if (action.toUpperCase() == "MOVE")
-                {
-                    if (components.length != 2)
+                    if (operator == null || target == null || 
+                        operator.type != TokenType.Skill_Operator || 
+                        target.type != TokenType.Alphabetic)
                     {
-                        return this.generateErrorResult("Input not in correct format. MOVE specified but no direction.");
+                        throw "ERROR P4";
                     }
-                    let target = components[1].trim();
 
-                    switch (target.toUpperCase())
+                    switch (target.token)
                     {
                         case "UP": {
-                            actions.push(new action_Move(0, -1));
-                            break;
+                            return new action_Move(0, -1);
                         }
                         case "DOWN": {
-                            actions.push(new action_Move(0, 1));
-                            break;
+                            return new action_Move(0, 1);
                         }
                         case "LEFT": {
-                            actions.push(new action_Move(-1, 0));
-                            break;
+                            return new action_Move(-1, 0);
                         }
                         case "RIGHT": {
-                            actions.push(new action_Move(1, 0));
-                            break;
+                            return new action_Move(1, 0);
                         }
                         default: {
-                            return this.generateErrorResult("Valid MOVE actions are UP,DOWN,LEFT,RIGHT");
+                            throw "ERROR P5";
+                        }
+                    }
+                }
+
+                if (GLOBAL_GAME.library.lookupSkillForClass(className, action.token))
+                {
+                    let operator:Token = null;
+                    let target:Token = null;
+                    if (tokens[seek] != null && tokens[seek].type == TokenType.Skill_Operator)
+                    {
+                        operator = tokens[seek++];
+                        target = tokens[seek++];
+    
+                        if (operator == null || target == null || 
+                            operator.type != TokenType.Skill_Operator || 
+                            target.type != TokenType.Alphabetic)
+                        {
+                            throw "ERROR P6";
                         }
                     }
 
-                    continue;
-                }
-                else if (GLOBAL_GAME.library.lookupSkillForClass(className, action))
-                {
-                    let skill = GLOBAL_GAME.library.lookupSkill(action);
+                    let skill = GLOBAL_GAME.library.lookupSkill(action.token);
                     if (skill.selfOnly || skill.allAllies)
                     {
-                        actions.push(new action_Skill(action, ["ally"]));
-                        continue;
-                    }
-
-                    if (components.length != 2)
-                    {
-                        return this.generateErrorResult(`No target specified for ${action}`);
+                        return new action_Skill(action.token, ["all"]);
                     }
                     
-                    let target = components[1].trim();
-                    if (!validTargets.includes(target.toUpperCase()))
+                    if (operator == null || target == null)
                     {
-                        return this.generateErrorResult(`Invalid target ${target} for ${action}`);
+                        throw "ERROR P7";
                     }
 
-                    actions.push(new action_Skill(action, [target]));
-                    continue;
+                    if (validTargets.includes(target.token))
+                    {
+                        return new action_Skill(action.token, [target.token])
+                    }
                 } 
-                else 
+
+                throw "ERROR P8";
+            }
+            
+            let process_loop = (): Action[] =>
+            {
+                let token1 = tokens[seek++]; // loop
+                let token2 = tokens[seek++]; // (
+                let token3 = tokens[seek++]; // 5
+                let token4 = tokens[seek++]; // )
+                let token5 = tokens[seek++]; // {
+
+                if (token1 == null || token1.type != TokenType.Loop || 
+                    token2 == null || token2.type != TokenType.Arg_Open || 
+                    token3 == null || token3.type != TokenType.Numeric || 
+                    token4 == null || token4.type != TokenType.Arg_Close || 
+                    token5 == null || token5.type != TokenType.Block_Open)
                 {
-                    return this.generateErrorResult(`Invalid action ${action}`);
+                    throw "ERROR L4";
+                }
+
+                let loopedActions: Action[] = [];
+                while (tokens[seek] != null && tokens[seek].type != TokenType.Block_Close)
+                {
+                    switch(tokens[seek].type)
+                    {
+                        case TokenType.Wait: {
+                            loopedActions.push(process_wait());
+                            break;
+                        }
+
+                        case TokenType.Alphabetic: {
+                            loopedActions.push(process_action());
+                            break;
+                        }
+
+                        default: {
+                            throw "ERROR L1";
+                        }
+                    }
+                }
+                
+                if (tokens[seek] == null)
+                {
+                    throw "ERROR L2";
+                }
+
+                seek++;
+
+                let totalActions: Action[] = [];
+                for (let i = 0; i < Number(token3.token); i++)
+                {
+                    totalActions = totalActions.concat(loopedActions);
+                }
+
+                return totalActions;
+            }
+
+
+            let actions: Action[] = [];
+            while (seek < tokens.length)
+            {
+                switch(tokens[seek].type)
+                {
+                    case TokenType.Wait: {
+                        actions.push(process_wait());
+                        break;
+                    }
+
+                    case TokenType.Loop: {
+                        actions = actions.concat(process_loop());
+                        break
+                    }
+
+                    case TokenType.Alphabetic: {
+                        actions.push(process_action());
+                        break;
+                    }
+
+                    default: {
+                        throw "ERROR B9";
+                    }
                 }
             }
 
-            return this.generateSuccessResult(actions);
+            for(let i = 0; i < actions.length; i++)
+            {
+                console.log(`b ${ActionType[actions[i].type]} name: ${actions[i].skill} target: ${actions[i].targets[0]}`);
+            }
+
+            return actions;
+        }
+
+        tokenize = (raw: string): Token[] => 
+        {
+            let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            let numerals = "0123456789"
+            let symbols = "(){}:";
+
+            let token_wait = "WAIT";
+            let token_loop = "LOOP";
+            let token_arg_open = "(";
+            let token_arg_close = ")";
+            let token_block_open = "{";
+            let token_block_close = "}";
+            let token_skill_operator = ":";
+
+            let seek = 0;
+            raw = raw.split("\r\n").join(" ").split("\r").join(" ").split("\n").join(" ").trim().toUpperCase();
+
+            let isWhiteSpace = (char:string) =>
+            {
+                return char == null || char == "" || char == " " || char == "\n" || char == "\r" || char == "\t";
+            }
+
+            let isAlphabetic = (char:string) =>
+            {
+                return alphabet.indexOf(char) >= 0;
+            }
+
+            let isNumeric = (char:string) =>
+            {
+                return numerals.indexOf(char) >= 0;
+            }
+
+            let isAlphaNumeric = (char:string) =>
+            {
+                return isAlphabetic(char) || isNumeric(char);
+            }
+
+            let isSymbol = (char:string) =>
+            {
+                return symbols.indexOf(char) >= 0;
+            }
+
+            let readToken = (): Token =>
+            {
+                let token = "";
+                let char = "";
+                let type: TokenType = null;
+
+                while (isWhiteSpace(char))
+                {
+                    char = raw[seek++];
+                }
+                
+                if (isSymbol(char))
+                {
+                    switch (char)
+                    {
+                        case token_block_open: {
+                            type = TokenType.Block_Open;
+                            break;
+                        }
+                        case token_block_close: {
+                            type = TokenType.Block_Close;
+                            break;
+                        }
+                        case token_arg_open: {
+                            type = TokenType.Arg_Open;
+                            break;
+                        }
+                        case token_arg_close: {
+                            type = TokenType.Arg_Close;
+                            break;
+                        }
+                        case token_skill_operator: {
+                            type = TokenType.Skill_Operator;
+                            break;
+                        }
+                    }
+                    
+                    return {token: char, type: type};
+                }
+
+                if (!isAlphaNumeric(char))
+                {
+                    throw "ERROR 1"
+                }
+
+                if (isAlphabetic(char))
+                {
+                    while (isAlphabetic(char))
+                    {
+                        token += char;
+                        char = raw[seek++];
+                    }
+
+                    seek--;
+
+                    switch (token)
+                    {
+                        case token_wait: {
+                            type = TokenType.Wait;
+                            break;
+                        }
+                        case token_loop: {
+                            type = TokenType.Loop;
+                            break;
+                        }
+                        default: {
+                            type = TokenType.Alphabetic;
+                            break;
+                        }
+                    }
+                }
+
+                else if (isNumeric(char))
+                {
+                    while (isNumeric(char))
+                    {
+                        token += char;
+                        char = raw[seek++];
+                    }
+    
+                    seek--;
+                    type = TokenType.Numeric;
+                }
+
+                return {token: token, type: type};
+            }
+
+            let tokens = [];
+            while (seek < raw.length)
+            {
+                let token = readToken();
+                tokens.push(token);
+                //console.log(`seek: ${seek} token: ${token.token} type: ${TokenType[token.type]}`);
+            }
+
+            return tokens;
         }
     }
 }
