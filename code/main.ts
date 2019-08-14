5d29643d2bfbe7175dd4ef865eea0e727c944e92
Nick Parisi <nsparisi@gmail.com>
2019-08-13 22:23:41 -0700

    spacebar can be used again

--- code/main.ts
+++ code/main.ts
@@ -117,7 +117,7 @@ document.body.addEventListener("keydown", (event) =>
     //console.log(`key: ${event.code} ${event.keyCode}`);
 
     // prevent arrow keys from manipulating the page
-    if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+    if([37, 38, 39, 40].indexOf(event.keyCode) > -1) {
         event.preventDefault();
     }
 
